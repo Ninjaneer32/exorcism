6e5468332acb6b8d52c1714a2f974bcd30d49600
Theodore Salvo <buzzsurfr@users.noreply.github.com>
2022-10-19 23:17:18 +0000

    First test, list daemonsets

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # exorcism
 Exorcism - Remove the Daemons!
+
+_Creating the example from https://dev.to/douglasmakey/implementing-a-simple-k8s-admission-controller-in-go-2dcg_
---
+++ pods/create.go
@@ -0,0 +1,83 @@
+package pods
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/buzzsurfr/exorcism"
+
+	"k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func validateCreate() exorcism.AdmitFunc {
+	return func(r *v1beta1.AdmissionRequest) (*exorcism.Result, error) {
+		pod, err := parsePod(r.Object.Raw)
+		if err != nil {
+			return &exorcism.Result{Msg: err.Error()}, nil
+		}
+
+		for _, c := range pod.Spec.Containers {
+			if strings.HasSuffix(c.Image, ":latest") {
+				return &exorcism.Result{Msg: "You cannot use the tag 'latest' in a container."}, nil
+			}
+		}
+
+		return &exorcism.Result{Allowed: true}, nil
+	}
+}
+
+func mutateCreate() exorcism.AdmitFunc {
+	return func(r *v1beta1.AdmissionRequest) (*exorcism.Result, error) {
+		// Get the list of DaemonSets
+
+		// creates the in-cluster config
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		// creates the clientset
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		daemonsets, err := clientset.AppsV1().DaemonSets("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		for _, daemonset := range daemonsets.Items {
+			fmt.Printf("%s\t%s\n", daemonset.Namespace, daemonset.Name)
+		}
+
+		var operations []exorcism.PatchOperation
+		// pod, err := parsePod(r.Object.Raw)
+		// if err != nil {
+		// 	return &exorcism.Result{Msg: err.Error()}, nil
+		// }
+
+		// Very simple logic to inject a new "sidecar" container.
+		// if pod.Namespace == "special" {
+		// 	var containers []v1.Container
+		// 	containers = append(containers, pod.Spec.Containers...)
+		// 	sideC := v1.Container{
+		// 		Name:    "test-sidecar",
+		// 		Image:   "busybox:stable",
+		// 		Command: []string{"sh", "-c", "while true; do echo 'I am a container injected by mutating webhook'; sleep 2; done"},
+		// 	}
+		// 	containers = append(containers, sideC)
+		// 	operations = append(operations, exorcism.ReplacePatchOperation("/spec/containers", containers))
+		// }
+
+		// Add a simple annotation using `AddPatchOperation`
+		// metadata := map[string]string{"origin": "fromMutation"}
+		// operations = append(operations, exorcism.AddPatchOperation("/metadata/annotations", metadata))
+		return &exorcism.Result{
+			Allowed:  true,
+			PatchOps: operations,
+		}, nil
+	}
+}
