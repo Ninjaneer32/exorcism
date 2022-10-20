25e469f557ee12359b5de6dc39857a05374404b4
Theodore Salvo <buzzsurfr@users.noreply.github.com>
2022-10-20 02:41:05 +0000

    correct cert svc name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ echo "Creating certificates"
 mkdir certs
 openssl req -nodes -new -x509 -keyout certs/ca.key -out certs/ca.crt -subj "/CN=Admission Controller Demo"
 openssl genrsa -out certs/admission-tls.key 2048
-openssl req -new -key certs/admission-tls.key -subj "/CN=admission-server.default.svc" | openssl x509 -req -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/admission-tls.crt
+openssl req -new -key certs/admission-tls.key -subj "/CN=exorcism.default.svc" | openssl x509 -req -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/admission-tls.crt
 
 echo "Creating k8s Secret"
 kubectl create secret tls admission-tls \
